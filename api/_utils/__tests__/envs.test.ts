56240b0d1d96ee1c1d6ff9e542ec46cb87b7d3b3
ff6347 <hash@ff6347.email>
2022-04-13 20:07:06 +0200

    test(springcleaning): update tests for the spring
    Nothing important

--- api/_utils/__tests__/envs.test.ts
+++ api/_utils/__tests__/envs.test.ts
@@ -1,10 +1,11 @@
+/* eslint-disable jest/no-conditional-expect */
 /* eslint-disable jest/require-to-throw-message */
 /* eslint-disable jest/valid-expect-in-promise */
 /* eslint-disable jest/no-hooks */
 describe("testing env values", () => {
   afterEach(() => {
     delete process.env.JWKS_URI;
-    delete process.env.AUTH0_AUDIENCE_FRONTEND;
+    // delete process.env.AUTH0_AUDIENCE_FRONTEND;
     delete process.env.AUTH0_AUDIENCE;
     delete process.env.AUTH0_ISSUER;
     delete process.env.AUTH0_CLIENT_ID_MANAGEMENT_API;
@@ -15,7 +16,7 @@ describe("testing env values", () => {
   });
   test("all values exist module does not throw", () => {
     process.env.JWKS_URI = "foo";
-    process.env.AUTH0_AUDIENCE_FRONTEND = "foo";
+    // process.env.AUTH0_AUDIENCE_FRONTEND = "foo";
     process.env.AUTH0_AUDIENCE = "foo";
     process.env.AUTH0_ISSUER = "foo";
     process.env.AUTH0_CLIENT_ID_MANAGEMENT_API = "foo";
@@ -35,31 +36,9 @@ describe("testing env values", () => {
       });
   });
 
-  test("should thorow error due to missing JWKS_URI", () => {
+  test("should thorow error due to missing JWS_URI", () => {
     // const origProcessEnv = process.env;
     // process.env.JWKS_URI = "foo";
-    process.env.AUTH0_AUDIENCE_FRONTEND = "foo";
-    process.env.AUTH0_AUDIENCE = "foo";
-    process.env.AUTH0_ISSUER = "foo";
-    process.env.AUTH0_CLIENT_ID_MANAGEMENT_API = "foo";
-    process.env.AUTH0_CLIENT_SECRET_MANAGEMENT_API = "foo";
-    process.env.AUTH0_AUDIENCE_MANAGEMENT_API_AUDIENCE = "foo";
-    process.env.AUTH0_TOKEN_API_URL_MANAGEMENT_API = "foo";
-    process.env.AUTH0_MANAGEMENT_API_URL = "foo";
-    import("../envs")
-      .then((module) => {
-        expect(module).toBeDefined();
-        expect(() => {
-          module.getEnvs();
-        }).toThrow();
-      })
-      .catch((err) => {
-        throw err;
-      });
-  });
-  test("should thorow error due to missing AUTH0_AUDIENCE_FRONTEND", () => {
-    // const origProcessEnv = process.env;
-    process.env.JWKS_URI = "foo";
     // process.env.AUTH0_AUDIENCE_FRONTEND = "foo";
     process.env.AUTH0_AUDIENCE = "foo";
     process.env.AUTH0_ISSUER = "foo";
@@ -79,10 +58,33 @@ describe("testing env values", () => {
         throw err;
       });
   });
+  // eslint-disable-next-line jest/no-commented-out-tests
+  // test("should thorow error due to missing AUTH0_AUDIENCE_FRONTEND", () => {
+  //   // const origProcessEnv = process.env;
+  //   process.env.JWKS_URI = "foo";
+  //   process.env.AUTH0_AUDIENCE_FRONTEND = "foo";
+  //   process.env.AUTH0_AUDIENCE = "foo";
+  //   process.env.AUTH0_ISSUER = "foo";
+  //   process.env.AUTH0_CLIENT_ID_MANAGEMENT_API = "foo";
+  //   process.env.AUTH0_CLIENT_SECRET_MANAGEMENT_API = "foo";
+  //   process.env.AUTH0_AUDIENCE_MANAGEMENT_API_AUDIENCE = "foo";
+  //   process.env.AUTH0_TOKEN_API_URL_MANAGEMENT_API = "foo";
+  //   process.env.AUTH0_MANAGEMENT_API_URL = "foo";
+  //   import("../envs")
+  //     .then((module) => {
+  //       expect(module).toBeDefined();
+  //       expect(() => {
+  //         module.getEnvs();
+  //       }).toThrow();
+  //     })
+  //     .catch((err) => {
+  //       throw err;
+  //     });
+  // });
   test("should thorow error due to missing AUTH0_AUDIENCE", () => {
     // const origProcessEnv = process.env;
     process.env.JWKS_URI = "foo";
-    process.env.AUTH0_AUDIENCE_FRONTEND = "foo";
+    // process.env.AUTH0_AUDIENCE_FRONTEND = "foo";
     // process.env.AUTH0_AUDIENCE = "foo";
     process.env.AUTH0_ISSUER = "foo";
     process.env.AUTH0_CLIENT_ID_MANAGEMENT_API = "foo";
@@ -104,7 +106,7 @@ describe("testing env values", () => {
   test("should thorow error due to missing AUTH0_ISSUER", () => {
     // const origProcessEnv = process.env;
     process.env.JWKS_URI = "foo";
-    process.env.AUTH0_AUDIENCE_FRONTEND = "foo";
+    // process.env.AUTH0_AUDIENCE_FRONTEND = "foo";
     process.env.AUTH0_AUDIENCE = "foo";
     // process.env.AUTH0_ISSUER = "foo";
     process.env.AUTH0_CLIENT_ID_MANAGEMENT_API = "foo";
@@ -126,7 +128,7 @@ describe("testing env values", () => {
   test("should thorow error due to missing AUTH0_CLIENT_ID_MANAGEMENT_API", () => {
     // const origProcessEnv = process.env;
     process.env.JWKS_URI = "foo";
-    process.env.AUTH0_AUDIENCE_FRONTEND = "foo";
+    // process.env.AUTH0_AUDIENCE_FRONTEND = "foo";
     process.env.AUTH0_AUDIENCE = "foo";
     process.env.AUTH0_ISSUER = "foo";
     // process.env.AUTH0_CLIENT_ID_MANAGEMENT_API = "foo";
@@ -148,7 +150,7 @@ describe("testing env values", () => {
   test("should thorow error due to missing AUTH0_CLIENT_SECRET_MANAGEMENT_API", () => {
     // const origProcessEnv = process.env;
     process.env.JWKS_URI = "foo";
-    process.env.AUTH0_AUDIENCE_FRONTEND = "foo";
+    // process.env.AUTH0_AUDIENCE_FRONTEND = "foo";
     process.env.AUTH0_AUDIENCE = "foo";
     process.env.AUTH0_ISSUER = "foo";
     process.env.AUTH0_CLIENT_ID_MANAGEMENT_API = "foo";
@@ -170,7 +172,7 @@ describe("testing env values", () => {
   test("should thorow error due to missing AUTH0_AUDIENCE_MANAGEMENT_API_AUDIENCE", () => {
     // const origProcessEnv = process.env;
     process.env.JWKS_URI = "foo";
-    process.env.AUTH0_AUDIENCE_FRONTEND = "foo";
+    // process.env.AUTH0_AUDIENCE_FRONTEND = "foo";
     process.env.AUTH0_AUDIENCE = "foo";
     process.env.AUTH0_ISSUER = "foo";
     process.env.AUTH0_CLIENT_ID_MANAGEMENT_API = "foo";
@@ -192,7 +194,7 @@ describe("testing env values", () => {
   test("should thorow error due to missing AUTH0_TOKEN_API_URL_MANAGEMENT_API", () => {
     // const origProcessEnv = process.env;
     process.env.JWKS_URI = "foo";
-    process.env.AUTH0_AUDIENCE_FRONTEND = "foo";
+    // process.env.AUTH0_AUDIENCE_FRONTEND = "foo";
     process.env.AUTH0_AUDIENCE = "foo";
     process.env.AUTH0_ISSUER = "foo";
     process.env.AUTH0_CLIENT_ID_MANAGEMENT_API = "foo";
@@ -214,7 +216,7 @@ describe("testing env values", () => {
   test("should thorow error due to missing AUTH0_MANAGEMENT_API_URL", () => {
     // const origProcessEnv = process.env;
     process.env.JWKS_URI = "foo";
-    process.env.AUTH0_AUDIENCE_FRONTEND = "foo";
+    // process.env.AUTH0_AUDIENCE_FRONTEND = "foo";
     process.env.AUTH0_AUDIENCE = "foo";
     process.env.AUTH0_ISSUER = "foo";
     process.env.AUTH0_CLIENT_ID_MANAGEMENT_API = "foo";
