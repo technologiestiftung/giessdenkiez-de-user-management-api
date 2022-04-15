309490d797ee3b124510efb931faf1e2ae99c4e1
ff6347 <hash@ff6347.email>
2022-04-15 15:24:40 +0200

    chore(deps): update more deps

--- api/_utils/__tests__/package.test.ts
+++ api/_utils/__tests__/package.test.ts
@@ -1,6 +1,4 @@
 import { getPackage } from "../package";
-import fs from "fs";
-const spyFsRead = jest.spyOn(fs, "readFileSync");
 describe("package module", () => {
   // eslint-disable-next-line jest/no-hooks
   afterAll(() => {
@@ -8,8 +6,6 @@ describe("package module", () => {
   });
   test("should", () => {
     const pkg = getPackage();
-    // eslint-disable-next-line jest/prefer-called-with
-    expect(spyFsRead).toHaveBeenCalled();
     expect(pkg.name).toBeDefined();
     expect(pkg.version).toBeDefined();
   });
